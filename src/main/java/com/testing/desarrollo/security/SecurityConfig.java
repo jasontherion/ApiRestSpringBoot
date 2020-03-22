2818b06b77d266edd27ff688c4b584142062c451
Jeyson <jasontherion@gmail.com>
2020-03-22 13:04:21 -0500

    u

--- src/main/java/com/testing/desarrollo/security/SecurityConfig.java
+++ src/main/java/com/testing/desarrollo/security/SecurityConfig.java
@@ -3,18 +3,26 @@ package com.testing.desarrollo.security;
 import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootProperties;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
+
 @KeycloakConfiguration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Import(KeycloakWebSecurityConfigurerAdapter.class)
 class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
     /**
